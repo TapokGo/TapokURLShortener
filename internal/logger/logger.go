d5748839d3da894df13ca2c70fac4005c1269741
60050 <C:\Users\user\AppData\Roaming\The Bat!>
2025-12-06 14:41:58 +0300

    docs(repo): fix docs

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -7,19 +7,19 @@ type Logger interface {
 	// Args must be provided as key-value pairs
 	Info(msg string, args ...any)
 
-	// Info logs a message at Warn level.
+	// Warn logs a message at Warn level.
 	// Args must be provided as key-value pairs
 	Warn(msg string, args ...any)
 
-	// Info logs a message at Error level.
+	// Error logs a message at Error level.
 	// Args must be provided as key-value pairs
 	Error(msg string, args ...any)
 
-	// Info logs a message at Debug level.
+	// Debug logs a message at Debug level.
 	// Args must be provided as key-value pairs
 	Debug(msg string, args ...any)
 
-	// Return logger with new context(static key-value pair).
+	// Return new logger with context(static key-value pair).
 	// Args must be a provided ad key-value pairs
 	With(args ...any) Logger
 }
