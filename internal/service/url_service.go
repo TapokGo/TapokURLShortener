dca6d0a5a4d5c12710d38a68e74457b2e5583e8a
Nikita Dunski <nikitadun0706@gmail.com>
2025-12-07 19:09:11 +0300

    feat(url_service): create base of the service layer
    - add future function description

--- internal/service/url_service.go
+++ internal/service/url_service.go
@@ -1,16 +1,39 @@
-// Package service temp
+// Package service provides utilities for create short URL by origin and get original URL by short
 package service
 
 import "github.com/Tapok-Go/TestURLShortener/internal/repo"
 
-// URLService temp
+// URLService is a model of url-shortener service layer
 type URLService struct {
 	repo repo.URLStorage
 }
 
-// NewURLService temp
+// NewURLService create the new URLService
 func NewURLService(s repo.URLStorage) *URLService {
 	return &URLService{
 		repo: s,
 	}
+}
+
+// CreateShortURL create short URL by origin URL
+func (u *URLService) CreateShortURL(originURL string) (string, error) {
+	// Validate URL
+
+	// Create alias
+
+	// Save pair to the db (and check what that short URL not already exist)
+
+	// return short url
+	return "", nil
+}
+
+// ResolveShortURL return shortURL original pair from db
+func (u *URLService) ResolveShortURL(shortURL string) (string, error) {
+	// Get short URL from db
+	return "", nil
+}
+
+func (u *URLService) generateAlias(originURL string) (string, error) {
+	// Create unique short URL base on original
+	return "", nil
 }
